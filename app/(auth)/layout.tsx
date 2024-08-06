e8bc6c132321ad9d73ba1cc026732a6ca79feb5f
Tanmay-312 <jain2002tj@gmail.com>
2024-08-06 16:17:08 +0530

    auth image updated

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,10 +11,10 @@ export default function RootLayout({
             <div className="auth-asset">
                 <div>
                     <Image 
-                        src="/icons/auth-image.svg"
+                        src="/icons/auth-image.jpg"
                         alt="Auth image"
-                        width={500}
-                        height={500}
+                        width={750}
+                        height={750}
                         className="rounded-l-xl object-contain"
                     />
                 </div>
